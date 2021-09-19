b2b9aec7126e3a413c1b29c70ca231c6a75ae8b7
Marta Martin Delgado <marta_martin93@hotmail.com>
2021-09-19 08:50:29 +0100

    Updated configuration string in App.config for business layer, may need to change

---
+++ DataLayer/DBAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DBAccess
+    {
+        SqlConnection conn;
+        public DBAccess()
+        {
+            conn = new SqlConnection("Data Source = 20.86.154.86; User ID = sa; Password = ********; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+        }
+    }
+}
